7ed01fd142a42e066322eb7091e2b1b88a97d67d
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-07-18 11:29:36 +0900

    add-toastr

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX }
 end
